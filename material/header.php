db4b9ab4a69ba414bc526d46409a97ecaf3037f7
HaruSari <uxdx159@gmail.com>
2020-05-02 17:52:59 +0900

    Update header.php

--- material/header.php
+++ material/header.php
@@ -17,7 +17,8 @@
         <header>
             <nav class="light-blue lighten-1" role="navigation">
 
-                <div class="nav-wrapper container"><a id="logo-container" href="<?php echo $rootUrl; ?>" class="brand-logo">뿌리 줄기</a>
+                <div class="nav-wrapper container"><a id="logo-container" href="https://harujapan.net"
+                        class="brand-logo">뿌리 줄기</a>
                     <!-- 프로그램 검색 메뉴 리스트 -->
                     <?php wp_nav_menu(array(
                         'container' => '',
@@ -35,11 +36,13 @@
                     <ul class="right hide-on-med-and-down">
                         <li>
                             <!-- 프로그램 검색 메뉴 -->
-                            <a class="dropdown-trigger" href="#!" data-target="dropdown-program-search">프로그램 검색<i class="material-icons right">arrow_drop_down</i></a>
+                            <a class="dropdown-trigger" href="#!" data-target="dropdown-program-search">프로그램 검색<i
+                                    class="material-icons right">arrow_drop_down</i></a>
                         </li>
                         <li>
                             <!-- 프로그램 설정 메뉴 -->
-                            <a class="dropdown-trigger" href="#!" data-target="dropdown-program-setting">프로그램 설정<i class="material-icons right">arrow_drop_down</i></a>
+                            <a class="dropdown-trigger" href="#!" data-target="dropdown-program-setting">프로그램 설정<i
+                                    class="material-icons right">arrow_drop_down</i></a>
                         </li>
                     </ul>
                     <!-- 모바일 환경 -->
